65cd3a97606aae6cb19d914f150c864122ef29a1
zain ul abideen <zainulabideen6996@gmail.com>
2024-12-15 08:20:19 +0500

    Add files via upload

---
+++ Assignment-07/code-11/script.js
@@ -0,0 +1,15 @@
+// Eligibility for Voting
+// Write a program that checks if a person is eligible to vote based on their age (age >= 18).
+function ageFunc() {
+    var numberInput = document.getElementById('numberInput').value;
+    var result = document.getElementById('outputResult');
+    let message;
+    if (numberInput >= 18) {
+        result.textContent = 'You are elegible to vote';
+        result.style.color = 'Green'
+    }
+    else{
+        result.textContent = 'You are not elegible to vote'
+        result.style.color = 'red'
+    }
+}
